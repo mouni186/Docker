510aa24c9a353ff8bc3e60951f6126dfd286f064
mouni186 <mounikas1861999@gmail.com>
2022-04-14 21:34:10 +0530

    feat:Implementation of docker images and container

---
+++ api/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+app.use(cors());
+
+
+
+app.get('/',(req,res) => {
+    res.json([
+        {
+            "id":"1",
+            "title":"THe bear"
+        },
+        {
+            "id":"2",
+            "title":"Pokemon"
+        }
+    ])
+})
+
+
+
+
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Listening on port`)
+})
